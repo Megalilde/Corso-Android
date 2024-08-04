d26252e891f3a91b1ee76efba3388cdfbcceb1d0
Megalilde <63415418+Megalilde@users.noreply.github.com>
2024-08-04 13:36:44 +0200

    aggiunto il timer per avviare gli esercizi ed  il timer per gli esercizi

--- 7MinutesWorkout/app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ 7MinutesWorkout/app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -1,8 +1,12 @@
 package com.example.a7minutesworkout
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.a7minutesworkout.databinding.ActivityExerciseBinding
@@ -11,6 +15,12 @@ import com.example.a7minutesworkout.databinding.ActivityMainBinding
 class ExerciseActivity : AppCompatActivity() {
 
     private var binding: ActivityExerciseBinding? = null
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress: Int = 0
+
+    private var exerciseTimer: CountDownTimer? = null
+    private var eserciseProgress: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -30,5 +40,83 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        setupRestView()
+
+    }
+
+    private fun setupRestView(){
+        if(restTimer!= null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+
+    private fun setupExerciseView(){
+        binding?.flProgressBar?.visibility = View.GONE
+
+        // Molto importante, permette di cambiare il constraint dinamicamente.
+        val layoutParams = binding?.tvTitle?.layoutParams as ConstraintLayout.LayoutParams
+        layoutParams.bottomToTop = binding?.flExerciseView!!.id
+        binding?.tvTitle?.text = "Exercise Name"
+        binding?.flExerciseView?.visibility = View.VISIBLE
+        if(exerciseTimer!= null){
+            exerciseTimer?.cancel()
+            eserciseProgress = 0
+        }
+        setExerciseProgressBar()
+    }
+
+
+    private fun setRestProgressBar(){
+        binding?.progressBar?.progress = restProgress
+
+        // Voglio che ogni 1000 millisecondi esegue il Metodo della durata totale di 10000 millisecondi
+        restTimer = object: CountDownTimer(10000,1000){
+            override fun onTick(p0: Long) {
+                restProgress++;
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,"Exercises will start now!",Toast.LENGTH_LONG).show()
+                setupExerciseView()
+            }
+
+        }.start()
+
+    }
+
+    private fun setExerciseProgressBar(){
+        binding?.progressBarExercise?.progress = eserciseProgress
+
+        // Voglio che ogni 1000 millisecondi esegue il Metodo della durata totale di 10000 millisecondi
+        exerciseTimer = object: CountDownTimer(30000,1000){
+            override fun onTick(p0: Long) {
+                eserciseProgress++;
+                binding?.progressBarExercise?.progress = 30 - eserciseProgress
+                binding?.tvTimerExercise?.text = (30 - eserciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,"Exercises is done!",Toast.LENGTH_LONG).show()
+            }
+
+        }.start()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(restTimer!= null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        if(exerciseTimer!= null){
+            exerciseTimer?.cancel()
+            eserciseProgress = 0
+        }
+        binding = null
     }
 }
