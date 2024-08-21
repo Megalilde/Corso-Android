29df966accacc976080d93d63a13e58c368a2584
Megalilde <63415418+Megalilde@users.noreply.github.com>
2024-08-21 16:25:11 +0200

    aggiunta del progetto nuovo,  splashscreen, e intro page

---
+++ Projemanag/app/src/main/java/com/example/projemanag/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.projemanag
+
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projemanag.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private var binding: ActivitySplashBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        // Setta a full screen
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        )
+
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
+        binding?.tvAppName?.typeface = typeFace
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        },2500)
+    }
+}
