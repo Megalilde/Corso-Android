7c2950d0ab08fa280202ad80e9bfe9820f1afa83
Megalilde <63415418+Megalilde@users.noreply.github.com>
2024-08-21 12:25:04 +0200

    aggiornata UI dell'app

--- MyQuizApp/app/src/main/java/com/example/myquizapp/activities/QuizQuestionsActivity.kt
+++ MyQuizApp/app/src/main/java/com/example/myquizapp/activities/QuizQuestionsActivity.kt
@@ -8,6 +8,7 @@ import android.os.CountDownTimer
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
@@ -48,10 +49,10 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         mUserName = intent.getStringExtra(Constants.USER_NAME)
         mUserSurname= intent.getStringExtra(Constants.USER_SURNAME)
 
-        binding?.tvOptionOne?.setOnClickListener(this)
-        binding?.tvOptionTwo?.setOnClickListener(this)
-        binding?.tvOptionTree?.setOnClickListener(this)
-        binding?.tvOptionFour?.setOnClickListener(this)
+        binding?.llOptionOne?.setOnClickListener(this)
+        binding?.llOptionTwo?.setOnClickListener(this)
+        binding?.llOptionThree?.setOnClickListener(this)
+        binding?.llOptionFour?.setOnClickListener(this)
         binding?.btnSubmit?.setOnClickListener(this)
 
         mQuestionList = QuestionList().getQuestions()
@@ -70,10 +71,10 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         binding?.tvQuestion?.text = question.question
         binding?.ivImage?.setImageResource(question.image)
         binding?.tvProgress?.text = "Question $mCurrentPosition/${mQuestionList?.size}"
-        binding?.tvOptionOne?.text = question.optionOne
-        binding?.tvOptionTwo?.text = question.optionTwo
-        binding?.tvOptionTree?.text = question.optionTree
-        binding?.tvOptionFour?.text = question.optionFour
+        binding?.tvOptionOneTemp?.text = question.optionOne
+        binding?.tvOptionTwoTemp?.text = question.optionTwo
+        binding?.tvOptionThreeTemp?.text = question.optionTree
+        binding?.tvOptionFourTemp?.text = question.optionFour
 
         if(mCurrentPosition == mQuestionList!!.size){
             binding?.btnSubmit?.text = "FINISH"
@@ -86,39 +87,98 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     // Setta al livello grafico le TextView delle varie opzioni nel caso di default (Grafica)
 
     private fun defaultOptionsView(){
-        val options = mutableListOf<TextView>()
-        //Esegui una funzione sull'oggetto considerato se esso non è vuoto, alla posizione 0 aggiunti esso.
-        binding?.tvOptionOne?.let {
-            options.add(0,it)
+        val optionsLinearLayout = mutableListOf<LinearLayout>()
+
+        binding?.llOptionOne?.let {
+            optionsLinearLayout.add(0,it)
         }
-        binding?.tvOptionTwo?.let {
-            options.add(1,it)
+        binding?.llOptionTwo?.let {
+            optionsLinearLayout.add(1,it)
         }
-        binding?.tvOptionTree?.let {
-            options.add(2,it)
+        binding?.llOptionThree?.let {
+            optionsLinearLayout.add(2,it)
         }
-        binding?.tvOptionFour?.let {
-            options.add(3,it)
+        binding?.llOptionFour?.let {
+            optionsLinearLayout.add(3,it)
         }
-        for(option in options){
-            // Setta gli stili delle TextView e background
-            option.setTextColor(Color.parseColor("#7A8089"))
-            option.typeface = Typeface.DEFAULT
+        for(option in optionsLinearLayout){
+            // Setta gli stili delle  background
             option.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
+        }
+
+        for(linearLayout in optionsLinearLayout){
+            // First child
+            val firstChild = linearLayout.getChildAt(0)
+
+            // Last child
+            val lastChild = linearLayout.getChildAt(linearLayout.childCount - 1)
+
+
+            if (firstChild is TextView) {
+                val optionTextViewFirst = mutableListOf<TextView>()
+                binding?.tvOptionLetterA?.let {
+                    optionTextViewFirst.add(0,it)
+                }
+                binding?.tvOptionLetterB?.let {
+                    optionTextViewFirst.add(1,it)
+                }
+                binding?.tvOptionLetterC?.let {
+                    optionTextViewFirst.add(2,it)
+                }
+                binding?.tvOptionLetterD?.let {
+                    optionTextViewFirst.add(3,it)
+                }
+                for(option in optionTextViewFirst){
+                    // Setta gli stili delle TextView e background
+                    option.setTextColor(Color.parseColor("#7A8089"))
+                    option.typeface = Typeface.DEFAULT
+                }
+
+            }
+
+            if (lastChild is TextView) {
+                val optionTextViewLast = mutableListOf<TextView>()
+                binding?.tvOptionOneTemp?.let {
+                    optionTextViewLast.add(0,it)
+                }
+                binding?.tvOptionTwoTemp?.let {
+                    optionTextViewLast.add(1,it)
+                }
+                binding?.tvOptionThreeTemp?.let {
+                    optionTextViewLast.add(2,it)
+                }
+                binding?.tvOptionFourTemp?.let {
+                    optionTextViewLast.add(3,it)
+                }
+                for(option in optionTextViewLast){
+                    // Setta gli stili delle TextView e background
+                    option.setTextColor(Color.parseColor("#7A8089"))
+                    option.typeface = Typeface.DEFAULT
+                }
+            }
+
         }
 
     }
 
 
     // Setta al livello grafico le TextView delle varie opzioni nel caso di selezionamento. (Grafica)
-    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int){
+    private fun selectedOptionView(ll: LinearLayout, selectedOptionNum: Int){
         defaultOptionsView()
 
-        // Setta gli stili delle TextView e background
         mSelectedOptionPosition = selectedOptionNum
-        tv.setTextColor(Color.parseColor("#363A43"))
-        tv.setTypeface(tv.typeface, Typeface.BOLD)
-        tv.background = ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
+        // First child
+        val firstChild = ll.getChildAt(0)
+
+        // Last child
+        val lastChild = ll.getChildAt(ll.childCount - 1)
+
+        (firstChild as TextView).setTextColor(Color.parseColor("#363A43"))
+        firstChild.setTypeface(firstChild.typeface, Typeface.BOLD)
+
+        (lastChild as TextView).setTextColor(Color.parseColor("#363A43"))
+        lastChild.setTypeface(firstChild.typeface, Typeface.BOLD)
+        ll.background = ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
 
 
     }
@@ -126,71 +186,71 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View?) {
         when(view?.id){
-            R.id.tvOptionOne -> {
-                binding?.tvOptionOne?.let {
+            R.id.llOptionOne -> {
+                binding?.llOptionOne?.let {
                     selectedOptionView(it,1)
                 }
             }
-            R.id.tvOptionTwo -> {
-                binding?.tvOptionTwo?.let {
+            R.id.llOptionTwo -> {
+                binding?.llOptionTwo?.let {
                     selectedOptionView(it,2)
                 }
             }
-            R.id.tvOptionTree -> {
-                binding?.tvOptionTree?.let {
+            R.id.llOptionThree -> {
+                binding?.llOptionThree?.let {
                     selectedOptionView(it,3)
                 }
             }
-            R.id.tvOptionFour -> {
-                binding?.tvOptionFour?.let {
+            R.id.llOptionFour -> {
+                binding?.llOptionFour?.let {
                     selectedOptionView(it,4)
                 }
             }
             R.id.btnSubmit ->{
 
-            if (mSelectedOptionPosition == 0) {
+                if (mSelectedOptionPosition == 0) {
 
-                mCurrentPosition++
+                    mCurrentPosition++
 
-                when {
+                    when {
 
-                    mCurrentPosition <= mQuestionList!!.size -> {
+                        mCurrentPosition <= mQuestionList!!.size -> {
 
-                        setQuestion()
+                            setQuestion()
+                        }
+                        else -> {
+                            val intent = Intent(this, ResultActivity::class.java)
+                            intent.putExtra(Constants.USER_NAME, mUserName)
+                            intent.putExtra(Constants.USER_SURNAME,mUserSurname)
+                            intent.putExtra(Constants.CORRECT_ANSWER, mCorrectAnswer)
+                            intent.putExtra(Constants.WRONG_ANSWER, mWrongAnswer)
+                            intent.putExtra(Constants.TOTAL_QUESTIONS, mQuestionList?.size)
+                            intent.putExtra(Constants.TOTAL_TIME, mCounterSeconds)
+                            startActivity(intent)
+                            finish()
+                        }
                     }
-                    else -> {
-                        val intent = Intent(this, ResultActivity::class.java)
-                        intent.putExtra(Constants.USER_NAME, mUserName)
-                        intent.putExtra(Constants.USER_SURNAME,mUserSurname)
-                        intent.putExtra(Constants.CORRECT_ANSWER, mCorrectAnswer)
-                        intent.putExtra(Constants.WRONG_ANSWER, mWrongAnswer)
-                        intent.putExtra(Constants.TOTAL_QUESTIONS, mQuestionList?.size)
-                        intent.putExtra(Constants.TOTAL_TIME, mCounterSeconds)
-                        startActivity(intent)
-                        finish()
-                    }
-                }
-            } else {
-                val question = mQuestionList?.get(mCurrentPosition - 1)
-
-                // Verifica se la domanda è quella corretta.
-                if (question!!.correctAnswer != mSelectedOptionPosition) {
-                    answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
-                    mWrongAnswer++
-                }else{
-                    mCorrectAnswer++
-                }
-
-                // Nel caso della risposta corretta
-                answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
-
-                if (mCurrentPosition == mQuestionList!!.size) {
-                    binding?.btnSubmit?.text = "FINISH"
                 } else {
-                    binding?.btnSubmit?.text = "GO TO NEXT QUESTION"
-                }
+                    val question = mQuestionList?.get(mCurrentPosition - 1)
 
-                mSelectedOptionPosition = 0
+                    // Verifica se la domanda è quella corretta.
+                    if (question!!.correctAnswer != mSelectedOptionPosition) {
+                        answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                        mWrongAnswer++
+                    }else{
+                        mCorrectAnswer++
+                    }
+
+                    // Nel caso della risposta corretta
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+                    if (mCurrentPosition == mQuestionList!!.size) {
+                        binding?.btnSubmit?.text = "FINISH"
+                    } else {
+                        binding?.btnSubmit?.text = "GO TO NEXT QUESTION"
+                    }
+
+                    mSelectedOptionPosition = 0
                 }
             }
         }
@@ -200,19 +260,20 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private fun answerView(answer:Int, drawableView: Int){
         when(answer){
             1 -> {
-                binding?.tvOptionOne?.background = ContextCompat.getDrawable(this,drawableView)
+                binding?.llOptionOne?.background = ContextCompat.getDrawable(this,drawableView)
+
 
             }
             2 -> {
-                binding?.tvOptionTwo?.background = ContextCompat.getDrawable(this,drawableView)
+                binding?.llOptionTwo?.background = ContextCompat.getDrawable(this,drawableView)
 
             }
             3 -> {
-                binding?.tvOptionTree?.background = ContextCompat.getDrawable(this,drawableView)
+                binding?.llOptionThree?.background = ContextCompat.getDrawable(this,drawableView)
 
             }
             4 -> {
-                binding?.tvOptionFour?.background = ContextCompat.getDrawable(this,drawableView)
+                binding?.llOptionFour?.background = ContextCompat.getDrawable(this,drawableView)
 
             }
         }
