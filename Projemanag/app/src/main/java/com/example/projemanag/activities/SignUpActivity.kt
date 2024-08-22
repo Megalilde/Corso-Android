f525bcc6b47800d126c1964052a12610b91379dd
Megalilde <63415418+Megalilde@users.noreply.github.com>
2024-08-22 11:24:37 +0200

    aggiunta ui registrazione e login, impostato firebase

---
+++ Projemanag/app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
@@ -0,0 +1,39 @@
+package com.example.projemanag.activities
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projemanag.R
+import com.example.projemanag.databinding.ActivitySignUpBinding
+
+
+class SignUpActivity : AppCompatActivity() {
+
+    private var binding: ActivitySignUpBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        )
+
+        setupActionBar()
+    }
+
+
+    private fun setupActionBar(){
+        setSupportActionBar(binding?.toolbarSignUpActivity)
+
+        val actionBar = supportActionBar
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+
+        binding?.toolbarSignUpActivity?.setNavigationOnClickListener { onBackPressed() }
+    }
+}
