a18a7a2f979cacdf6319edf115fe75d0d694a3d6
Megalilde <63415418+Megalilde@users.noreply.github.com>
2024-08-23 15:35:28 +0200

    aggiunta la funzionalita del drawer e del selezionamento degli item.

--- Projemanag/app/src/main/java/com/example/projemanag/activities/MainActivity.kt
+++ Projemanag/app/src/main/java/com/example/projemanag/activities/MainActivity.kt
@@ -1,12 +1,84 @@
 package com.example.projemanag.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import com.example.projemanag.R
+import com.example.projemanag.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding?.root)
+
+        setupActionBar()
+
+
+        binding?.navView?.setNavigationItemSelectedListener(this)
+    }
+
+
+    // Settiamo l'icona hamburger e il tap su di esso.
+    private fun setupActionBar(){
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_main_activity)
+        setSupportActionBar(findViewById(R.id.toolbar_main_activity))
+        toolbar.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+
+        toolbar.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    // Serve per far scomparire e apparire il Drawer
+    private fun toggleDrawer(){
+        if(binding?.drawerLayout?.isDrawerOpen(GravityCompat.START) == true){
+            binding?.drawerLayout?.closeDrawer(GravityCompat.START)
+        }else{
+            binding?.drawerLayout?.openDrawer(GravityCompat.START)
+        }
+    }
+
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if(binding?.drawerLayout?.isDrawerOpen(GravityCompat.START) == true){
+            binding?.drawerLayout?.closeDrawer(GravityCompat.START)
+        }else{
+            doubleBackToExit()
+        }
+
+    }
+
+    // Implementando l'interfaccia NavigationView.OnNavigationItemSelectedListener ci permette implementare questo metodo
+    // Nota: Item sono uno dei due item che abbiamo aggiunto ovvero profilo e sign out
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_my_profile ->{
+                // Aggiungere intent qui per andare alla pagina del profilo.
+                Toast.makeText(this, "My Profile", Toast.LENGTH_LONG).show()
+            }
+            R.id.nav_sign_out ->{
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+
+        }
+        binding?.drawerLayout?.closeDrawer(GravityCompat.START)
+        return true
     }
 }
