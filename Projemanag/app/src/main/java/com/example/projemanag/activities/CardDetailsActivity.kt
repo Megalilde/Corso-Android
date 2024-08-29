3452f5d4806d111dd9582d0b3ca58b2d0ffbe653
Megalilde <63415418+Megalilde@users.noreply.github.com>
2024-08-29 12:53:35 +0200

    setup cardDetails

--- Projemanag/app/src/main/java/com/example/projemanag/activities/CardDetailsActivity.kt
+++ Projemanag/app/src/main/java/com/example/projemanag/activities/CardDetailsActivity.kt
@@ -5,16 +5,50 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.projemanag.R
 import com.example.projemanag.databinding.ActivityCardDetailsBinding
 import com.example.projemanag.databinding.ActivityMembersBinding
+import com.example.projemanag.models.Board
+import com.example.projemanag.utils.Constants
 
 class CardDetailsActivity : AppCompatActivity() {
 
     private var binding: ActivityCardDetailsBinding? = null
+
+    private lateinit var mBoardDetails: Board
+    private var mTaskListPosition = -1
+    private var mCardPosition = -1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCardDetailsBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+        getIntentData()
+        setupActionBar()
 
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(binding?.toolbarCardDetailsActivity)
+
+        val actionBar = supportActionBar
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+            actionBar.title = mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name
+        }
+
+        binding?.toolbarCardDetailsActivity?.setNavigationOnClickListener { onBackPressed() }
+    }
+
+
+    private fun getIntentData(){
+        if(intent.hasExtra(Constants.BOARD_DETAIL)){
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
+        }
+        if(intent.hasExtra(Constants.TASK_LIST_ITEM_POSITION)){
+            mTaskListPosition = intent.getIntExtra(Constants.TASK_LIST_ITEM_POSITION, -1)
+        }
+        if(intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)){
+            mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
+        }
     }
 }
--- Projemanag/app/src/main/java/com/example/projemanag/utils/Constants.kt
+++ Projemanag/app/src/main/java/com/example/projemanag/utils/Constants.kt
@@ -30,6 +30,9 @@ object Constants {
 
     const val EMAIL: String = "email"
 
+    const val TASK_LIST_ITEM_POSITION: String = "task_list_item_position"
+    const val CARD_LIST_ITEM_POSITION: String = "card_list_item_position"
+
     // Metodo relativo ai permessi dello storage.
     fun showImageChooser(activity: Activity){
         val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
