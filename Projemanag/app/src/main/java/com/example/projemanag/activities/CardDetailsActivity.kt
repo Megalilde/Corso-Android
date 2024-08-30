71d8750f152669c96c0bd146ec10034509850326
Megalilde <63415418+Megalilde@users.noreply.github.com>
2024-08-30 10:53:50 +0200

    aggiunto funzionalità color della card

--- Projemanag/app/src/main/java/com/example/projemanag/activities/CardDetailsActivity.kt
+++ Projemanag/app/src/main/java/com/example/projemanag/activities/CardDetailsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.projemanag.activities
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.projemanag.R
 import com.example.projemanag.databinding.ActivityCardDetailsBinding
 import com.example.projemanag.databinding.ActivityMembersBinding
+import com.example.projemanag.dialogs.LabelColorListDialog
 import com.example.projemanag.firebase.FirestoreClass
 import com.example.projemanag.models.Board
 import com.example.projemanag.models.Card
@@ -24,6 +26,8 @@ class CardDetailsActivity : BaseActivity() {
     private var mTaskListPosition = -1
     private var mCardPosition = -1
 
+    private var mSelectedColor = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,12 +39,21 @@ class CardDetailsActivity : BaseActivity() {
         binding?.etNameCardDetails?.setText(mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name)
         binding?.etNameCardDetails?.setSelection(binding?.etNameCardDetails?.text?.length ?: 0)
 
+        mSelectedColor = mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].labelColor
+        if(mSelectedColor.isNotEmpty()){
+            setColor()
+        }
+
         binding?.btnUpdateCardDetails?.setOnClickListener {
             if(binding?.etNameCardDetails?.text.toString().isNotEmpty()){
                 updateCardDetails()
             }else{
                 Toast.makeText(this,"Enter a card name.", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        binding?.tvSelectLabelColor?.setOnClickListener {
+            labelColorsListDialog()
         }
     }
 
@@ -67,6 +80,24 @@ class CardDetailsActivity : BaseActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_delete_card,menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun colorsList(): ArrayList<String>{
+        val colorsList: ArrayList<String> = ArrayList()
+        colorsList.add("#43C86F")
+        colorsList.add("#0C90F1")
+        colorsList.add("#F72400")
+        colorsList.add("#7A8089")
+        colorsList.add("#D57C1D")
+        colorsList.add("#770000")
+        colorsList.add("#0022F8")
+
+        return colorsList
+    }
+
+    private fun setColor(){
+        binding?.tvSelectLabelColor?.text = ""
+        binding?.tvSelectLabelColor?.setBackgroundColor(Color.parseColor(mSelectedColor))
     }
 
 
@@ -98,7 +129,8 @@ class CardDetailsActivity : BaseActivity() {
         val card = Card(
             binding?.etNameCardDetails?.text.toString(),
             mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].createdBy,
-            mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].assignedTo
+            mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].assignedTo,
+            mSelectedColor
         )
         mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition] = card
         showProgressDialog(resources.getString(R.string.please_wait))
@@ -137,5 +169,24 @@ class CardDetailsActivity : BaseActivity() {
         val alertDialog: AlertDialog = builder.create()
         alertDialog.setCancelable(false)
         alertDialog.show()
+    }
+
+    private fun labelColorsListDialog(){
+        val colorsList: ArrayList<String> = colorsList()
+
+        val listDialog = object : LabelColorListDialog(
+            this,
+            colorsList,
+            resources.getString(R.string.str_select_label_color),
+            mSelectedColor){
+            override fun onItemSelected(color: String) {
+                mSelectedColor = color
+                setColor()
+            }
+
+
+        }
+        listDialog.show()
+
     }
 }
--- Projemanag/app/src/main/java/com/example/projemanag/adapters/CardListItemsAdapter.kt
+++ Projemanag/app/src/main/java/com/example/projemanag/adapters/CardListItemsAdapter.kt
@@ -1,15 +1,13 @@
 package com.example.projemanag.adapters
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projemanag.databinding.ItemCardBinding
-import com.example.projemanag.databinding.ItemTaskBinding
-
 import com.example.projemanag.models.Card
-import com.example.projemanag.models.Task
 
 open class CardListItemsAdapter(private val context: Context, private var list: ArrayList<Card>): RecyclerView.Adapter<CardListItemsAdapter.MyViewHolder>() {
 
@@ -30,6 +28,12 @@ open class CardListItemsAdapter(private val context: Context, private var list: 
         val model = list[position]
         val binding = holder.binding
 
+        if(model.labelColor.isNotEmpty()){
+            binding.viewLabelColor.visibility = View.VISIBLE
+            binding.viewLabelColor.setBackgroundColor(Color.parseColor(model.labelColor))
+        }else{
+            binding.viewLabelColor.visibility = View.GONE
+        }
         binding.tvCardName.text = model.name
 
         holder.itemView.setOnClickListener{
