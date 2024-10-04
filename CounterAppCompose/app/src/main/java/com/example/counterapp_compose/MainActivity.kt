8f66458bc51d67fd4ffc4856a9a7f5db99da7980
Megalilde <63415418+Megalilde@users.noreply.github.com>
2024-10-04 16:27:41 +0200

    aggiunto mvvm e repository

--- CounterAppCompose/app/src/main/java/com/example/counterapp_compose/MainActivity.kt
+++ CounterAppCompose/app/src/main/java/com/example/counterapp_compose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,16 +13,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
+
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.counterapp_compose.ui.theme.CounterAppComposeTheme
@@ -31,11 +29,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viewModel: CounterViewModel by viewModels()
             CounterAppComposeTheme {
                 Surface(modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp)) {
-                    TheCounterApp()
+                    TheCounterApp(viewModel)
                 }
             }
         }
@@ -43,24 +42,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TheCounterApp(){
-    val count = remember { mutableStateOf(0) }
-
-    fun increment(){
-        count.value++
-    }
-    fun decrement(){
-        count.value--
-    }
-
+fun TheCounterApp(viewModel: CounterViewModel){
     Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text("Count ${count.value}", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Text("Count ${viewModel.count.value}", fontSize = 24.sp, fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.height(16.dp))
         Row {
-            Button(onClick = {increment()}) {
+            Button(onClick = {viewModel.increment()}) {
                 Text(text = "Increment")
             }
-            Button(onClick = {decrement()}) {
+            Button(onClick = {viewModel.decrement()}) {
                 Text(text = "Decrement")
 
             }
